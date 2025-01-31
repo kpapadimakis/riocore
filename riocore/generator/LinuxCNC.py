b5536f4f1a35a52f2773af3509e915d01e76fee9
Kostas Papadimakis <k.papadimakis@gmail.com>
2025-01-31 20:56:51 +0200

    modify speed divider for working with dupont wires for initial testing, this resulted in very little comm errors

--- riocore/generator/LinuxCNC.py
+++ riocore/generator/LinuxCNC.py
@@ -2363,7 +2363,7 @@ class LinuxCNC:
         defines["SPI_PIN_MISO"] = "9"
         defines["SPI_PIN_CLK"] = "11"
         defines["SPI_PIN_CS"] = "8"  # CE1 = 7
-        defines["SPI_SPEED"] = "BCM2835_SPI_CLOCK_DIVIDER_256"
+        defines["SPI_SPEED"] = "BCM2835_SPI_CLOCK_DIVIDER_32768"
 
         for header in header_list:
             output.append(f"#include <{header}>")
